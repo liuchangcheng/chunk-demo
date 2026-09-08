fb2b18d71fc0d908621367917bc365b865b89078
liuchangcheng <36373384@qq.com>
2026-09-08 17:10:37 +0800

    Create eslint.config.js

---
+++ .circleci/eslint.config.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    files: ['**/*.js'],
+    ignores: ['node_modules/**'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+    },
+  },
+];
